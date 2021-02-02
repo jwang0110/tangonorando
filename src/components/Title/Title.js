ef5836945f848718218bf02c82c9921918d4ce42
jwang0110 <johnw7@uci.edu>
2021-02-01 21:32:36 -0800

    added responsive title

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,12 +1,34 @@
+import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 
+const useStyles = makeStyles((theme) => ({
+	title: {
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "2rem",
+		},
+	},
+	subtitle: {
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "0.8rem",
+		},
+	},
+}));
+
 const Title = () => {
+	const classes = useStyles();
+
 	return (
 		<>
-			<Typography variant="subtitle1" color="textPrimary" align="right">
+			<Typography
+				className={classes.subtitle}
+				variant="subtitle1"
+				color="textPrimary"
+				align="right"
+			>
 				単語のランド
 			</Typography>
 			<Typography
+				className={classes.title}
 				variant="h1"
 				color="textPrimary"
 				align="center"
